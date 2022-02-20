a4a321987b9a2caa3f399ef0bf478fd79047c49e
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2022-02-19 21:07:00 -0700

    Add button to clear and resize grid

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const container = document.querySelector('#container');
+
 function createGrid(size) {
-    const container = document.querySelector('#container');
     for (let i = 1; i <= size; i++) {
         for (let j = 1; j <= size; j++) {
             const square = document.createElement('div');
@@ -16,4 +17,14 @@ function hoverOnSquare(e) {
     this.style.backgroundColor = "black";
 }
 
+function clearGrid(e) {
+    container.innerHTML = "";
+    let answer = parseInt(window.prompt("Please enter the size of the new grid. (Max 100)", "16"));
+    answer = Math.min(answer, 100)
+    createGrid(answer);
+}
+
 createGrid(16);
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clearGrid);
